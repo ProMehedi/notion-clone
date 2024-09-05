34d026aaaa8a6fce5c0484770b20f8116bd60751
ProMehedi <admin@promehedi.com>
2024-09-06 00:04:28 +0600

    Add create-document convex function

---
+++ convex/documents.ts
@@ -0,0 +1,26 @@
+import { v } from 'convex/values'
+//
+import { mutation } from './_generated/server'
+
+export const create = mutation({
+  args: {
+    title: v.string(),
+    parentDocument: v.optional(v.id('documents')),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity()
+    if (!identity) throw new Error('Unauthenticated')
+
+    const userId = identity.subject
+
+    const document = await ctx.db.insert('documents', {
+      title: args.title,
+      userId,
+      parentDocument: args.parentDocument,
+      isArchived: false,
+      isPublished: false,
+    })
+
+    return document
+  },
+})
